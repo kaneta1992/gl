7ad05cf0cf7531dbce1e946d676b123d89cf3e0c
Shota Yoshihara <yoshihara-s@klab.com>
2018-02-20 21:51:50 +0900

    不要なコードを消す

--- main.go
+++ main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"image"
-	"image/draw"
-	_ "image/png"
 	"log"
-	"os"
 	"runtime"
 
 	"github.com/go-gl/gl/v3.3-core/gl"
-	"github.com/go-gl/glfw/v3.2/glfw"
 	"github.com/go-gl/mathgl/mgl32"
 	"github.com/kaneta1992/gl/renderer/gl33"
 )
@@ -45,7 +39,7 @@ func main() {
 	material.SetInt1ByName("tex", 0)
 
 	// Load the texture
-	texture, err := newTexture("square.png")
+	texture, err := gl33.NewTexture("square.png")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -58,67 +52,25 @@ func main() {
 	gl.ClearColor(1.0, 1.0, 1.0, 1.0)
 
 	angle := 0.0
-	previousTime := glfw.GetTime()
-
-	win.GameLoop(func() {
+	win.GameLoop(func(dt float64) {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
 		// Update
-		time := glfw.GetTime()
-		elapsed := time - previousTime
-		previousTime = time
-
-		angle += elapsed
+		angle += dt
 		model = mgl32.HomogRotate3D(float32(angle), mgl32.Vec3{0, 1, 0})
 
 		// Render
 		material.ShouldUseMaterial()
 		material.SetMatrix4ByName("model", model)
 
-		gl.ActiveTexture(gl.TEXTURE0)
-		gl.BindTexture(gl.TEXTURE_2D, texture)
+		texture.Set(0)
 
 		mesh.Draw()
 	})
-	win.Destory()
-}
-
-func newTexture(file string) (uint32, error) {
-	imgFile, err := os.Open(file)
-	if err != nil {
-		return 0, fmt.Errorf("texture %q not found on disk: %v", file, err)
-	}
-	img, _, err := image.Decode(imgFile)
-	if err != nil {
-		return 0, err
-	}
-
-	rgba := image.NewRGBA(img.Bounds())
-	if rgba.Stride != rgba.Rect.Size().X*4 {
-		return 0, fmt.Errorf("unsupported stride")
-	}
-	draw.Draw(rgba, rgba.Bounds(), img, image.Point{0, 0}, draw.Src)
-
-	var texture uint32
-	gl.GenTextures(1, &texture)
-	gl.ActiveTexture(gl.TEXTURE0)
-	gl.BindTexture(gl.TEXTURE_2D, texture)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-	gl.TexImage2D(
-		gl.TEXTURE_2D,
-		0,
-		gl.RGBA,
-		int32(rgba.Rect.Size().X),
-		int32(rgba.Rect.Size().Y),
-		0,
-		gl.RGBA,
-		gl.UNSIGNED_BYTE,
-		gl.Ptr(rgba.Pix))
-
-	return texture, nil
+	win.Delete()
+	mesh.Delete()
+	material.Delete()
+	texture.Delete()
 }
 
 var vertexShader = `
