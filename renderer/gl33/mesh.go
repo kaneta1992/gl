ed76f84a0cbd44e45cbeae758f78094a15e47c90
Shota Yoshihara <yoshihara-s@klab.com>
2018-02-20 20:41:38 +0900

    メッシュ作る

---
+++ renderer/gl33/mesh.go
@@ -0,0 +1,33 @@
+package gl33
+
+import "github.com/go-gl/gl/v3.3-core/gl"
+
+type Mesh struct {
+	vboId uint32
+	vaoId uint32
+}
+
+func NewMesh(vertices []float32, material *Material) (*Mesh, error) {
+	mesh := &Mesh{}
+	// Configure the vertex data
+	gl.GenVertexArrays(1, &mesh.vaoId)
+	gl.BindVertexArray(mesh.vaoId)
+
+	gl.GenBuffers(1, &mesh.vboId)
+	gl.BindBuffer(gl.ARRAY_BUFFER, mesh.vboId)
+	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
+
+	vertAttrib := uint32(gl.GetAttribLocation(material.id, gl.Str("vert\x00")))
+	gl.EnableVertexAttribArray(vertAttrib)
+	gl.VertexAttribPointer(vertAttrib, 3, gl.FLOAT, false, 5*4, gl.PtrOffset(0))
+
+	texCoordAttrib := uint32(gl.GetAttribLocation(material.id, gl.Str("vertTexCoord\x00")))
+	gl.EnableVertexAttribArray(texCoordAttrib)
+	gl.VertexAttribPointer(texCoordAttrib, 2, gl.FLOAT, false, 5*4, gl.PtrOffset(3*4))
+	return mesh, nil
+}
+
+func (m *Mesh) Draw() {
+	gl.BindVertexArray(m.vaoId)
+	gl.DrawArrays(gl.TRIANGLES, 0, 6*2*3)
+}
